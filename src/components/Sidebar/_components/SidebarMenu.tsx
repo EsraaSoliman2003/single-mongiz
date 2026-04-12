d8a5a477b44ba78fd262e5cdb79e2cbde6428f34
Esraa Soliman <ee341110@gmail.com>
2026-04-12 11:45:09 +0200

    remove multi vendors

--- src/components/Sidebar/_components/SidebarMenu.tsx
+++ src/components/Sidebar/_components/SidebarMenu.tsx
@@ -14,6 +14,7 @@ import {
   HiOutlineTicket,
   HiOutlineStar,
   HiOutlineClipboardList,
+  HiOutlineShoppingBag,
 } from "react-icons/hi";
 import { useTranslations } from "next-intl";
 import { useAppDispatch, useAppSelector } from "@/rtk/hooks";
@@ -82,6 +83,11 @@ export default function SidebarMenu({
       name: t("categories"),
       icon: <HiOutlineCollection className="text-xl" />,
       path: `/admin/categories`,
+    },
+    {
+      name: t("products"),
+      icon: <HiOutlineShoppingBag className="text-xl" />,
+      path: `/seller/products`,
     },
     {
       name: t("Orders"),
